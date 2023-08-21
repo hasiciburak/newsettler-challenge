04fadb1b92360af644bc1de4230f18e532fac389
Burak <burakhasici@gmail.com>
2023-08-21 23:52:33 +0300

    first commit

---
+++ src/components/StayUpdated/index.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import successIcon from "../../assets/icon-success.svg";
+const StayUpdated = ({ setMailSubmitted }) => {
+  const preIcon = <img src={successIcon} width={30} />;
+  return (
+    <div className="card-container">
+      {/* LEFT COLUMN */}
+      <div className="card-left-col">
+        <h1 className="stay-updated">Stay updated!</h1>
+        <p className="join-text">
+          Join 60,000+ product managers receiving monthly updates on:
+        </p>
+        <ul>
+          <li>
+            {preIcon}
+            Product discovery and building what matters
+          </li>
+          <li>
+            {preIcon}
+            Measuring to ensure updates are a success
+          </li>
+          <li>
+            {preIcon}
+            And much more!
+          </li>
+        </ul>
+
+        <div className="email-address">
+          <label htmlFor="">Email address</label>
+          <input type="text" placeholder="email@company.com" />
+        </div>
+
+        <div className="subscribe-area">
+          <button onClick={() => setMailSubmitted((pre) => !pre)}>
+            Subscribe to monthly newsettler
+          </button>
+        </div>
+      </div>
+      {/* RIGHT COLUMN */}
+      <div className="card-right-col">
+        <div className="card-image"></div>
+      </div>
+    </div>
+  );
+};
+
+export default StayUpdated;
---
+++ src/components/ThankYou/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import successImg from "../../assets/icon-success.svg";
+const ThankYou = ({ setMailSubmitted }) => {
+  return (
+    <div className="thank-you-container">
+      <img src={successImg} alt="" />
+      <h1 className="thanks">Thanks for subscribing!</h1>
+      <p className="confirmation">
+        A confirmation email has been sent to ash@loremcompany.com. Please open
+        it and click the button inside to confirm your subscription.
+      </p>
+      <button
+        className="dismiss-btn"
+        onClick={() => setMailSubmitted((pre) => !pre)}
+      >
+        Dismiss message
+      </button>
+    </div>
+  );
+};
+
+export default ThankYou;
